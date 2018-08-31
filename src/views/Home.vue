7eeb01ca70687c3db0d72712be8c14df8dc35e33
Jon Cron <jonnmb@gmail.com>
2018-08-30 18:56:24 -0600

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<img src="@/assets/hades.jpg" style="width: 100%;"
+			title="Hades the magnificent" alt="Hades the magnificent" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+	name: 'home',
 };
 </script>
+
+<style lang="scss" scoped>
+	.home {
+		width: 100%;
+		margin: 0 auto;
+		height: 100%;
+	}
+</style>
+
